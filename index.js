96a262b69e1b088a58b355beeb183e176a5d3719
Shujian CHEN <csj244911099@gmail.com>
2021-12-13 16:59:16 -0500

    init mogodb

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //CommonJS
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require('./services/passport');
+
+mongoose.connect(keys.mongoURL)
 
 const app = express();
 
-//route handler
-app.get('/',(req,res)=>{
-    res.send({bye:'there'});
-})
+require('./routes/authRoute')(app);
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
